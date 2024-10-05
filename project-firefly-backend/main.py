39462843ce7cb52120eed7b3e215debb8e8f6f4e
Justin Abuyuan <abuyuanjustin@gmail.com>
2024-10-05 15:47:42 -0400

    initialize backend flask

--- project-firefly-backend/main.py
+++ project-firefly-backend/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello Project Firefly!")
+from flask import Flask, Response
+
+import cv2
+
+# Stream data
+stream_url = "rtmp://172.20.10.10:1935/live"  # TODO: Put this in a gitignore
 
 
-if __name__ == "__main__":
-    main()
+def generate_frames():
+    cap = cv2.VideoCapture(stream_url)
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            raise ValueError("Frame not successfully read.")
+
+        # Encoding frame into JPEG
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+# Flask
+app = Flask(__name__)
+
+
+# Endpoints
+@app.route("/fire-feed")
+def video_feed():
+    print("Showing video feed:")
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
